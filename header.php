ac57bd2f6ef263f7be1380b99a0446956068096c
Shohrab Khan <shohrab0000@gmail.com>
2026-08-19 09:45:21 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,84 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Shohrab - Full Stack Developer Portfolio</title>
+    <link rel="icon" href="images/logo-light/fav.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="AIvent" name="description">
+    <meta content="" name="keywords">
+    <meta content="" name="author">
+    <!-- CSS Files
+    ================================================== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap">
+    <link href="css/vendors.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- color scheme -->
+    <link id="colors" href="css/colors/scheme-01.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom Logo Width -->
+    <style>
+    @media (min-width: 992px) {
+        :root {
+            --logo-width: 200px !important;
+        }
+    }
+    </style>
+</head>
+
+<body class="dark-scheme">
+
+    <div id="wrapper">
+
+        <div class="float-text show-on-scroll">
+            <span><a href="#">Scroll to top</a></span>
+        </div>
+        <div class="scrollbar-v show-on-scroll"></div>
+
+        <!-- page preloader begin -->
+        <div id="de-loader"></div>
+        <!-- page preloader close -->
+
+        <header class="transparent">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="de-flex">
+                            <div class="de-flex-col">
+                                <!-- logo begin -->
+                                <div id="logo">
+                                    <a href="index.php">
+                                        <img class="logo-main" src="images/logo-light/logo%20(3).png" alt="Shohrab">
+                                        <img class="logo-scroll" src="images/logo-light/logo%20(3).png" alt="Shohrab">
+                                        <img class="logo-mobile" src="images/logo-light/logo%20(3).png" alt="Shohrab">
+                                    </a>
+                                </div>
+                                <!-- logo close -->
+                            </div>
+
+                            <div class="de-flex-col">
+                                <div class="de-flex-col header-col-mid">
+                                    <ul id="mainmenu">
+                                        <li><a class="menu-item active" href="index.php#section-hero">Home</a></li>
+                                        <li><a class="menu-item" href="index.php#section-about">About</a></li>
+                                        <li><a class="menu-item" href="index.php#section-experience">Experience</a></li>
+                                        <li><a class="menu-item" href="index.php#section-skills">Skills</a></li>
+                                        <li><a class="menu-item" href="index.php#section-projects">Projects</a></li>
+                                        <li><a class="menu-item" href="index.php#section-education">Education</a></li>
+                                    </ul>
+                                </div>
+
+                                <div class="de-flex-col">
+                                    <a class="btn-main fx-slide w-100" href="index.php#section-contact"><span>Contact
+                                            Me</span></a>
+
+                                    <div class="menu_side_area">
+                                        <span id="menu-btn"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </header>
